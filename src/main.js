76271c4d664700a253de1efa5484897fb258c161
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-08-29 11:48:53 -0300

    Bootstrap / vue loader sass / vue session

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource';
+import VueSession from 'vue-session'
+import VueStyleLoader from 'vue-style-loader'
 
+Vue.use(VueSession)
+Vue.use(VueResource);
+Vue.use(VueStyleLoader);
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+/* global mixin */
+Vue.mixin({
+    data: function () {
+        return {
+            db_url: "http://localhost:8015/"
+        }
+    },
+    methods: {
+    }
+});
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>',
 })
