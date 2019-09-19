938bd2ddcd369c4a904612b4f7656e6147a71f28
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-09-19 15:44:26 -0300

    Organização das rotas

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueStyleLoader from 'vue-style-loader'
 import { SimpleAlerts } from "@filipedp/simple_dialog";
 import { SimpleConfirms } from "@filipedp/simple_dialog";
 import VueSession from 'vue-session'
-import router from './router'
+import router from './routes'
 import App from './App'
 var $ = require('jquery');
 window.$ = $;
